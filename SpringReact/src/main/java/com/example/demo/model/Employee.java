7ad1d321bd3b5a164f732c91fdcb91e3dd7a8acd
Idiatou Diallo <idiatou.diallo@inwpartners.com>
2021-10-29 16:20:48 +0000

    creation du domaine et du repository

---
+++ SpringReact/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name= "employees")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String emailId;
+	
+}
